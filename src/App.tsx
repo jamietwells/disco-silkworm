5870dd3e753e647f28f59c68248cd56028ee9664
Jamie Twells <jtwells3@googlemail.com>
2019-04-23 22:30:04 +0100

    parsing csproj files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, ChangeEvent } from 'react';
+import * as xml from 'xml2js'
 import './App.css';
 
-class App extends Component {
+const ReadFiles = (props: { children: string, onLoad: (file: File, content: string) => void, multiple?: boolean, accept?: string, className?: string, id?: string }) => {
+  type ICollection<T> = {
+    length: number;
+    [index: number]: T;
+  }
+
+  function ToArray<TItem>(collection: ICollection<TItem>) {
+    return ToArrayAny(collection, c => c.length, (c, i) => c[i]);
+  }
+
+  type LengthSelector<T> = (collection: T) => number;
+  type ItemSelector<T, TResult> = (collection: T, index: number) => TResult;
+
+  function ToArrayAny<T, TResult>(collection: T, lengthSelector: LengthSelector<T>, itemSelector: ItemSelector<T, TResult>): TResult[] {
+    const result: TResult[] = [];
+    for (let i = 0; i < lengthSelector(collection); i++)
+      result.push(itemSelector(collection, i));
+    return result;
+  }
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    if (event.currentTarget.files)
+      ToArray(event.currentTarget.files)
+        .map(f => ({
+          File: f,
+          Reader: new FileReader()
+        }))
+        .forEach(i => {
+          i.Reader.addEventListener("loadend", () => props.onLoad(i.File, (i.Reader.result as string)));
+          i.Reader.readAsText(i.File);
+        });
+  }
+
+  let inputElement: HTMLInputElement | null;
+  return <>
+    <input type='file' id='folder' hidden={true} className='input-file hidden' accept={props.accept} onChange={handleChange} multiple={props.multiple} ref={input => inputElement = input} />
+    <button id={props.id} className={props.className} onClick={() => {
+      if (inputElement)
+        (inputElement).click();
+    }}>{props.children}</button>
+  </>;
+}
+
+class App extends Component<{}, {Files: Array<{ Name: string, Json: string, Xml: string }> }> {
+  constructor(props: {}){
+    super(props);
+    this.state = {Files: [] };
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const instance = this;
+
+    function onLoad(file: File, content: string) {
+      xml.parseString(content, (e, r) => {
+        if (e)
+          console.error(e);
+        if (r)
+          instance.setState(old => { old.Files.push({Name: file.name, Json: JSON.stringify(r, null, 2), Xml:content }); return old; });
+      });
+    };
+
+    return <>
+      <ReadFiles onLoad={onLoad} multiple={true} accept='.csproj,.fsproj'>Import Project Files</ReadFiles>
+      <ul>{this.state.Files.map(f => <li>{f.Name}<pre>{f.Xml}</pre><pre>{f.Json}</pre></li>)}</ul>
+      </>
   }
 }
 
