cc09950f970d27ea3266b6430b0bfed8c36a1456
Jamie Twells <jtwells3@googlemail.com>
2019-04-25 21:42:08 +0100

    Simple graph and table added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { Component, ChangeEvent } from 'react';
+import * as vis from 'vis';
 import * as xml from 'xml2js'
 import './App.css';
+import { VisOptions } from './VisOptions';
 
 const ReadFiles = (props: { children: string, onLoad: (file: File, content: string) => void, multiple?: boolean, accept?: string, className?: string, id?: string }) => {
   type ICollection<T> = {
@@ -45,28 +47,150 @@ const ReadFiles = (props: { children: string, onLoad: (file: File, content: stri
   </>;
 }
 
-class App extends Component<{}, {Files: Array<{ Name: string, Json: string, Xml: string }> }> {
-  constructor(props: {}){
+type State = {
+  Files: FileInfo[],
+  ShowGraph: boolean
+}
+
+type FileInfo = {
+  Name: string,
+  Json: string,
+  Xml: string,
+  Parsed: ProjectFile
+}
+
+type ProjectFile = {
+  Project: {
+    $: {
+      Sdk: string;
+    };
+    ItemGroup: Array<{
+      ProjectReference: Array<{
+        $: {
+          Include: string;
+        };
+      }>;
+    }>;
+    PropertyGroup: Array<{
+      OutputType: string[];
+      TargetFramework: string[];
+    }>;
+  };
+}
+
+class App extends Component<{}, State> {
+
+  readonly accept: string;
+  elem: HTMLElement | null;
+  model: { nodes: any, edges: any } | null;
+
+  constructor(props: {}) {
     super(props);
-    this.state = {Files: [] };
+    this.state = { Files: [], ShowGraph: true };
+    this.accept = '.csproj,.fsproj,.proj,.vbproj'
+    this.elem = null;
+    this.model = null;
+    this.componentDidUpdate.bind(this);
+  }
+
+  componentDidUpdate() {
+    if (this.elem && this.model) {
+      const data = { nodes: new vis.DataSet(this.model.nodes), edges: new vis.DataSet(this.model.edges) };
+      new vis.Network(this.elem, data, VisOptions);
+    }
   }
 
   render() {
+
+    type FilesMapType = { File: FileInfo, ProjectReferences: string[] };
+    function ToDependencyMap(files: FileInfo[]): FilesMapType[] {
+      function ToDependencyMapInner(file: FileInfo): FilesMapType {
+        if(!file.Parsed.Project.ItemGroup)
+          return { File: file, ProjectReferences: [] }
+        const references = file
+          .Parsed
+          .Project
+          .ItemGroup
+          .filter(i => i.ProjectReference)
+          .map(i => i.ProjectReference)
+          .flat(1)
+          .filter(r => r.$.Include)
+          .map(r => r.$.Include);
+
+        return { File: file, ProjectReferences: references }
+      }
+      return files.map(ToDependencyMapInner);
+    }
+
     const instance = this;
 
     function onLoad(file: File, content: string) {
       xml.parseString(content, (e, r) => {
         if (e)
           console.error(e);
-        if (r)
-          instance.setState(old => { old.Files.push({Name: file.name, Json: JSON.stringify(r, null, 2), Xml:content }); return old; });
+        if (r) {
+          instance.setState(old => ({
+            ... old,
+            Files: old.Files.filter(f => f.Name !== file.name).concat({
+              Name: file.name,
+              Json: JSON.stringify(r, null, 2),
+              Xml: content,
+              Parsed: r
+            })
+          }));
+        }
       });
     };
 
+    const filesMap = ToDependencyMap(this.state.Files);
+
+    function mapEdges(f: FilesMapType) {
+      function mapReferences(r: string) {
+        const fileName = r.split('\\').pop()!.split('/').pop();
+        if(!fileName)
+          throw "fileName is null";
+        return ({ from: f.File.Name, to: fileName })
+      }
+      return f.ProjectReferences.map(mapReferences);
+    }
+    this.model = {
+      nodes: filesMap.map(f => ({ id: f.File.Name, label: f.File.Name })),
+      edges: filesMap.map(mapEdges).flat(1),
+    };
+
+    function onClick(){
+      instance.setState(old => ({ ... old, ShowGraph: !old.ShowGraph }));
+    }
+
     return <>
-      <ReadFiles onLoad={onLoad} multiple={true} accept='.csproj,.fsproj'>Import Project Files</ReadFiles>
-      <ul>{this.state.Files.map(f => <li>{f.Name}<pre>{f.Xml}</pre><pre>{f.Json}</pre></li>)}</ul>
-      </>
+      <ReadFiles onLoad={onLoad} multiple={true} accept={this.accept}>Import Project Files</ReadFiles>
+      <button onClick={onClick}>{instance.state.ShowGraph ? "Show table" : "Show graph"}</button>
+      <div hidden={!instance.state.ShowGraph} className="graph" ref={r => this.elem = r}></div>
+      <table hidden={instance.state.ShowGraph}>
+        <thead>
+          <tr>
+            <td>
+              File
+            </td>
+            <td>
+              References
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {filesMap.map(f => <tr key={f.File.Name}>
+            <td>
+              {f.File.Name}
+            </td>
+            <td>
+              <ul>
+                {f.ProjectReferences.map(r => <li key={r}>{r}</li>)}
+              </ul>
+            </td>
+          </tr>)}
+        </tbody>
+      </table>
+    </>
   }
 }
 
---
+++ src/VisOptions.ts
@@ -0,0 +1,22 @@
+import { Options } from "vis";
+
+export const VisOptions = {
+    autoResize: true,
+    edges: {
+        arrows: {
+            to: true,
+            from: false,
+            middle: false
+        }
+    },
+    nodes: {
+        color: {
+            background: "#101010",
+            border: "#E0E0E0",
+            highlight: "#303030"
+        },
+        font: {
+            color: "#E0E0E0"
+        }
+    }
+} as Options;
