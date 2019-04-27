eb43f9407637890172598d915ca28f15654f9366
Jamie Twells <jtwells3@googlemail.com>
2019-04-27 18:42:46 +0100

    Adding links to GitHub and Glitch

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { TableHTMLAttributes } from 'react';
 
 type TableProps<T> = {
     Columns: Array<ColumnDefinition<T>>;
     Data: T[];
-}
+} & React.DetailedHTMLProps<React.TableHTMLAttributes<HTMLTableElement>, HTMLTableElement>
 
 export class Table<T> extends React.Component<TableProps<T>, TableState<T>> {
     constructor(props: TableProps<T>){
@@ -16,9 +16,9 @@ export class Table<T> extends React.Component<TableProps<T>, TableState<T>> {
             Direction?: 'ASC' | 'DESC'
         }
 
-        const props = this.props;
+        const { Columns, Data, ...tableProps } = this.props;
 
-        const columns = props.Columns.map(c => c as SortableColumnDefinition<T>);
+        const columns = Columns.map(c => c as SortableColumnDefinition<T>);
         const instance = this;
 
         function getSortButton(column: SortableColumnDefinition<T>){
@@ -35,17 +35,17 @@ export class Table<T> extends React.Component<TableProps<T>, TableState<T>> {
             return <button onClick={onClick}>{column.Direction ? column.Direction === 'ASC' ? '↓' : '↑' : '-'}</button> 
         }
 
-        function mapData(row: T) {
-            return <tr>{columns.map(c => <td>{c.Content(row)}</td>)}</tr>;
+        function mapData(row: T, index: number) {
+            return <tr key={index}>{columns.map((c, i) => <td key={i}>{c.Content(row)}</td>)}</tr>;
         }
-        return <table>
+        return <table { ... tableProps }>
             <thead>
                 <tr>
-                    {columns.map(c => <td>{c.Name}{getSortButton(c)}</td>)}
+                    {columns.map((c, i) => <td key={i}>{c.Name}{getSortButton(c)}</td>)}
                 </tr>
             </thead>
             <tbody>
-                {props.Data.sort(instance.state.SortFunction).map(mapData)}
+                {Data.sort(instance.state.SortFunction).map(mapData)}
             </tbody>
         </table>;
     }
@@ -74,8 +74,6 @@ export class TableData<T> {
         this.Columns.push({ Name: name, Content: content } as ColumnDefinition<T>);
         return this;
     }
-
-
 
     AddSortableColumn<TResult extends selectorResult>(name: string, content: (rowItem: T) => TResult, sortFunction?: ((a: T, b: T) => number) ) {
         const fn =  sortFunction ? sortFunction : (a: T, b: T) => standardSort(content(a), content(b));
