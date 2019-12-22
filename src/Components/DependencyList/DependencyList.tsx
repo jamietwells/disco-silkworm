9cf3f8d4bc652364dc2308dedcc24a20aa2b129e
Jamie Twells <jtwells3@gmail.com>
2019-12-22 15:02:57 +0000

    List collapsed by default for performance and stack overflows

--- src/Components/DependencyList/DependencyList.tsx
+++ src/Components/DependencyList/DependencyList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FilesMapType } from '../../Helpers/DependencyMapper';
 import { ToDictionary } from '../../Helpers/helpers';
 import './DependencyList.css';
@@ -12,6 +12,21 @@ export function DependencyList(props: { files: FilesMapType[] }) {
 }
 
 function File(props: { currentFile: FilesMapType, files: Map<string, FilesMapType> }) {
+    const [isExpanded, setExpanded] = useState(false);
     const { currentFile, files } = props;
-    return <li>{currentFile.name}<ul>{currentFile.References.map(f => <File currentFile={files.get(f.path.raw)!} files={files} />)}</ul></li>
+    return <li><ToggleButton initialState={isExpanded} onStateChange={setExpanded}/> {currentFile.name}<ul>{currentFile.References.filter(_ => isExpanded).map(f => <File currentFile={files.get(f.path.raw)!} files={files} />)}</ul></li>
+}
+
+function ToggleButton(props: { initialState: boolean,  label?:{ Open: string, Closed: string }, onStateChange: (state: boolean) => void }) {
+    const [state, setOpenClosed] = useState(props.initialState);
+    const labels = (props.label || { Open: '-', Closed: '+' });
+    function SetState(newState: boolean){
+        return function(){
+            props.onStateChange(newState);
+            setOpenClosed(newState);
+        }
+    }
+    if(state)
+        return <button onClick={SetState(false)}>{labels.Open}</button>    
+    return <button onClick={SetState(true)}>{labels.Closed}</button>
 }
